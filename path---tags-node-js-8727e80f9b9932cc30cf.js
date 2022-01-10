01c3ca2aba4f720db74ba17bfc13b0039a30d585
chinloong <chinloong91@gmail.com>
2022-01-10 17:33:13 +0800

    Updates

---
+++ path---tags-node-js-8727e80f9b9932cc30cf.js
@@ -0,0 +1,2 @@
+webpackJsonp([75715161491867],{515:function(e,a){e.exports={data:{allMarkdownRemark:{totalCount:1,edges:[{node:{id:"/Users/chinloong/makespace/chinloongtan-web/src/pages/2022/01/10-pm2-commands/index.md absPath of file >>> MarkdownRemark",timeToRead:1,frontmatter:{title:"PM2 commands for your node.js backend",date:"January 10, 2022",path:"/blog/pm2-commands-nodejs-backend",excerpt:"Essentials for deployment",tags:["node.js"]}}}]}},pathContext:{tag:"node.js"}}}});
+//# sourceMappingURL=path---tags-node-js-8727e80f9b9932cc30cf.js.map
